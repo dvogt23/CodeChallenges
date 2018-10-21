e4265bca26c5c5177072816c9a1ac24ad2b8e481
dvogt23 <divogt@live.de>
2018-10-21 23:23:24 +0200

    better pythagorean triplet solution

--- exercism/rust/pythagorean-triplet/src/lib.rs
+++ exercism/rust/pythagorean-triplet/src/lib.rs
@@ -1,9 +1,14 @@
 pub fn find() -> Option<u32> {
-    for a in 1..1000 {
-        for b in 1..1000 {
-            if (a as f64 + b as f64 + ((a*a) as f64 + (b*b) as f64).sqrt()) == 1000.0f64 {
-                println!("a: {} b: {}", a, b);
-                return Some((a as f64 * b as f64 * ((a*a + b*b) as f64).sqrt()) as u32)
+    let max = 1000;
+    for a in 1..max {
+        for b in 1..max {
+            for c in 1..max {
+                if a*a + b*b - c*c == 0 {
+                    if a + b + c == 1000 {
+                        //println!("a:{} b:{} c:{}", a, b, c);
+                        return Some((a * b * c) as u32)
+                    }
+                } 
             }
         }
     }
