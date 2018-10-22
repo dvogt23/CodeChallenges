be4848ca04d38a96a5d9a08017c977bc743761d7
dvogt23 <divogt@live.de>
2018-10-22 08:30:07 +0200

    fix pythagorean trplet solution

--- exercism/rust/pythagorean-triplet/src/lib.rs
+++ exercism/rust/pythagorean-triplet/src/lib.rs
@@ -1,16 +1,13 @@
 pub fn find() -> Option<u32> {
     let max = 1000;
     for a in 1..max {
-        for b in 1..max {
-            for c in 1..max {
-                if a*a + b*b - c*c == 0 {
-                    if a + b + c == 1000 {
-                        //println!("a:{} b:{} c:{}", a, b, c);
-                        return Some((a * b * c) as u32)
-                    }
-                } 
+        for b in a+1..max {
+            let c = max - a -b;
+            if a*a + b*b - c*c == 0 {
+                println!("a:{} b:{} c:{}", a, b, c);
+                return Some((a * b * c) as u32)
             }
-        }
+        } 
     }
     None
 }
