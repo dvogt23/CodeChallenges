6ba1b1a6c11dfd816e02342ca09beb94f518a55b
dvogt23 <divogt@live.de>
2018-12-17 20:39:03 +0100

    exercsim/rust: fix saddle points

--- exercism/rust/saddle-points/src/lib.rs
+++ exercism/rust/saddle-points/src/lib.rs
@@ -1,35 +1,20 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    let mut result: Vec<(usize, usize)> = vec![];
+    let height = input.len();
+    let width = input.first().unwrap().len();
 
-    // iter rows
-    for i in 0..input.len() {
-        let row = &*input.iter().nth(i).unwrap();
-        let max_in_row = row.iter().max();
-
-        if max_in_row.is_none() {
-            return result
-        }
-
-        for j in 0..row.len() {
-
-            let current_value = row.iter().nth(j).unwrap();
-
-            if max_in_row.unwrap() != current_value {
-                continue
-            }
-
-            // min in max index column
-            let min_in_col = input.iter()
-                .map(|row| row.iter().nth(j).unwrap())
-                .min().unwrap();
-
-            // saddle if max == min
-            // add saddle point to result vec
-            if current_value == min_in_col {
-                result.push((i, j));
+    for (i, row) in input.iter().enumerate() {
+        let max_in_row = row.iter().max().unwrap();
+        for (j, col) in row.iter().enumerate() {
+            if col == max_in_row {
+                let lowest_in_col = input.iter().map(|row| row.iter().nth(j));
+                println!("low: {:?}", lowest_in_col);
             }
         }
+        
+        //println!("max {:?} i: {:?}", max_in_row, i);
     }
+    
+    println!("{}, {:?}", height, width);
 
-    result
+    vec![(0,0)]
 }
