27d01d404b8592aff0750e168d2ff7a001b5949a
dvogt23 <divogt@live.de>
2019-11-24 21:36:10 +0100

    exercism/rust: raindrops

---
+++ exercism/rust/raindrops/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn raindrops(n: u32) -> String {
+  let mut output = String::new();
+
+  if n % 3 == 0 {
+    output += "Pling";
+  }
+  if n % 5 == 0 {
+    output += "Plang";
+  }
+  if n % 7 == 0 {
+    output += "Plong";
+  }
+
+  if output == "" {
+    return n.to_string();
+  } else {
+    return output;
+  }
+}
